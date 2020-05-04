329f2f0c8101da95522efd50ca5c182a2aa4071c
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-05-04 20:19:34 +0200

    close

--- runLinux.sh
+++ runLinux.sh
@@ -4,11 +4,11 @@
  #a good practice to specify a folder for a specific build type
  # -S . is the new, it's old equivalent is -H. must be '-H.' in power shell
 # cmake -S . -B_builds\make -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
-cmake -S . -B_builds/make
+cmake -S . -B_builds/make -DCMAKE_CXX_STANDARD=11
 
 cmake --build ./_builds/make
 
-./_builds\make\changeT
+./_builds/make/changeT
 
 cd ./_builds/make
 
