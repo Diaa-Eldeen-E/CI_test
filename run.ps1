d4a5806e7edaf911edf4e41edcd7b8218f869f77
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-05-04 17:27:40 +0200

    second time

--- run.ps1
+++ run.ps1
@@ -3,7 +3,7 @@
  # .(CMakelists files dir), -B(build directory), -G (generate)
  #a good practice to specify a folder for a specific build type
  # -S . is the new, it's old equivalent is -H. must be '-H.' in power shell
-cmake -S . -B_builds\make -G "MinGW Makefiles"
+cmake -S . -B_builds\make -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
 
 cmake --build .\_builds\make
  #mingw32-make change -C .\_builds\make
