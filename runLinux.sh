ec99c102ce792921882ffe16c0235d6af457d1d6
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-05-05 05:55:49 +0200

    moved test to a dedicated folder

--- runLinux.sh
+++ runLinux.sh
@@ -4,9 +4,9 @@
  #a good practice to specify a folder for a specific build type
  # -S . is the new, it's old equivalent is -H. must be '-H.' in power shell
 # cmake -S . -B_builds\make -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
-cmake -S . -B_builds/make #-DCMAKE_CXX_STANDARD=11
+cmake -S . -B_builds/make
 
 cmake --build ./_builds/make
 
-./_builds/make/changeT
+./_builds/make/testChange
 
