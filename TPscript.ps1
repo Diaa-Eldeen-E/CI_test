2b68569a81cdeadd2db077b6c89726e0d55888de
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-05-06 06:42:17 +0200

    Added codecoverage sub module

--- TPscript.ps1
+++ TPscript.ps1
@@ -6,7 +6,7 @@ echo "Executing the script"
  # .(CMakelists files dir), -B(build directory), -G (generate)
  #a good practice to specify a folder for a specific build type
  # -S . is the new, it's old equivalent is -H. must be '-H.' in power shell
-#  cmake -S . -B_builds\make -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
+  cmake -S . -B_builds\make -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
 
 cmake --build .\_builds\make
  #mingw32-make change -C .\_builds\make
